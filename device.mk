65bad2b3d453213e7e52313c0c38063bc2fc39e1
infinixs5lite <kelvinchinedu97@gmail.com>
2024-03-18 22:04:19 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/tecno/TECNO-KG5k
+LOCAL_PATH := device/tecno/KG5k
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
